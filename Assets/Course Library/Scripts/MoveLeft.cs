331f0a3e17d404582a1f7202c5b828b767914396
jaredbest <me@jaredbest.com>
2021-03-06 23:21:14 +0100

    Finished lesson

---
+++ Assets/Course Library/Scripts/MoveLeft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    PlayerController playerControllerScript;
+
+    float speed = 30;
+    float leftBound = -15;
+
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+            Destroy(gameObject);
+    }
+}
